62c3973aa6be1576d941e8d748b6c648fda7b3ba
ChitraChauhan <chitrasureshchauhan@gmail.com>
2018-09-17 11:50:15 +0530

    demo app like axelor-web overview : initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import RestAPI from './rest-api';
+import FormContainer from './FormContainer';
+import ListContainer from './ListContainer'
+class App extends Component {
+
+componentDidMount() {
+    const restAPI = new RestAPI();
+    console.log('restAPI', restAPI)
+    restAPI.login('admin', 'admin').then(res => {
+      
+    }); 
+}  
+
+render() {
+  return (
+      <div className="App">
+        <FormContainer/>
+        <ListContainer/>
+      </div>
+    );
+  }
+}
+
+export default App;
+//"proxy": "http://sos.axelor.com:8081"
